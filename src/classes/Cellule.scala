8139cb0458aa635c91c7a7f2714906cc38da4b92
lamacrym0 <lamastico2.0@gmail.com>
2024-05-28 15:21:47 +0200

    Add function move to grid

--- src/classes/Cellule.scala
+++ src/classes/Cellule.scala
@@ -1,5 +1,9 @@
 package classes
 
-class Cellule (var isObstacl: Boolean = false, var haveSquatter: Boolean = false, var valueInt: Int = 0)  {
-
+class Cellule (var isObstacl: Boolean = false, var haveSquatter: Boolean = false, private var valueInt: Int = 0)  {
+  def getValueInt:Int = valueInt
+  def setValueInt(value:Int):Unit = {
+    valueInt = value
+    haveSquatter = true
+  }
 }
--- src/classes/Grid.scala
+++ src/classes/Grid.scala
@@ -3,41 +3,79 @@ package classes
 import scala.collection.mutable.ArrayBuffer
 
 object test extends App{
-  var grid:Grid = new Grid(2,3)
+  var grid:Grid = new Grid(3,2)
+  grid.display()
+  grid.move(North())
+  grid.display()
+  grid.move(East())
+  grid.display()
+  grid.move(South())
+  grid.display()
+  grid.move(West())
   grid.display()
 }
 class Grid (x:Int,y:Int) {
   var grid:ArrayBuffer[ArrayBuffer[Cellule]] = ArrayBuffer()
 
 
-  for( idx <- 0 until  x){
+  for( idy <- 0 until  y){
     grid.addOne(new ArrayBuffer[Cellule]())
-    for(idy <- 0 until y){
-      grid(idx).addOne(new Cellule())
+    for(idx <- 0 until x){
+      grid(idy).addOne(new Cellule())
     }
   }
+  grid(1)(0).setValueInt(1)
+
 
   def display(): Unit = {
-    for(x <- grid.indices ){
-      for (y<- grid(x).indices ){
-        if(grid(x)(y).isObstacl){
-          if(grid(x)(y).haveSquatter){
-            print("1 ")
-
+    for(y <- grid.indices ){
+      for (x<- grid(y).indices ){
+        if(!grid(y)(x).isObstacl){
+          if(grid(y)(x).haveSquatter){
+            print(grid(y)(x).getValueInt + " ")
           }else {
             print("0 ")
-
           }
         }
         else{
           print("X ")
         }
-
       }
       println()
     }
   }
 
+  def move(action: Direction): Unit = {
+    var headX: Int = getHeadPos()(1)
+    var headY: Int = getHeadPos()(0)
+
+    if(action.actionX + headX >= grid(0).length || action.actionX + headX < 0 || action.actionY + headY >= grid.length || action.actionY + headY < 0 ) {
+      return
+    }
+
+    if(grid(headY + action.actionY)(headX + action.actionX).isObstacl || grid(headY + action.actionY)(headX + action.actionX).haveSquatter)
+      return
+
+
+    grid(headY + action.actionY)(headX + action.actionX).setValueInt(grid(headY)(headX).getValueInt + 1)
+    move(action)
+  }
+
+  def getHeadPos: Array[Int] = {
+    var res: Array[Int] = Array(0, 0)
+    var maxVal: Int = 0;
+    for (y <- grid.indices) {
+      for (x <- grid(y).indices if (grid(y)(x).getValueInt > maxVal)) {
+
+        maxVal = grid(y)(x).getValueInt
+        res = Array(y, x)
+      }
+    }
+    res
+  }
+
 
 
 }
+
+
